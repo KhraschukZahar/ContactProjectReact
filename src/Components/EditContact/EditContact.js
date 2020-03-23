15136569fe8ff4e579650fbd3d74634bd86e3559
KhraschukZahar <antisocialll@ukr.net>
2020-03-23 23:04:23 +0200

    added project on git

---
+++ src/Components/EditContact/EditContact.js
@@ -0,0 +1,139 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+import "./EditContact.css";
+
+class EditContact extends React.Component {
+  state = {
+    id: this.props.currentContact.id,
+    name: this.props.currentContact.name,
+    address: this.props.currentContact.address,
+    phone: this.props.currentContact.phone,
+    email: this.props.currentContact.email,
+    gender: this.props.currentContact.gender,
+    avatar: this.props.currentContact.avatar,
+    star: this.props.currentContact.star,
+    isHome: false
+  };
+
+  getName = event => {
+    this.setState({
+      name: event.target.value
+    });
+  };
+  getName = event => {
+    this.setState({
+      name: event.target.value
+    });
+  };
+  getAddress = event => {
+    this.setState({
+      address: event.target.value
+    });
+  };
+  getPhone = event => {
+    this.setState({
+      phone: event.target.value
+    });
+  };
+  getAvatar = event => {
+    this.setState({
+      avatar: event.target.value
+    });
+  };
+  getEmail = event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
+  onSendData = event => {
+    event.preventDefault();
+    const { id, name, address, phone, avatar, email, isHome } = this.state;
+    this.props.onEditCurrentContact(name, address, phone, avatar, email, id);
+    this.setState({
+      isHome: true
+    });
+  };
+
+  render() {
+    //console.log("Inside EditContact ", this.props.currentContact);
+    const { name, address, phone, email, gender, avatar, isHome } = this.state;
+    if (isHome) {
+      return <Redirect to="/" />;
+    }
+
+    return (
+      <div>
+        <form onSubmit={this.onSendData}>
+          <div className="form-group">
+            <div className="form-group">
+              <label forhtml="name">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                onChange={this.getName}
+                placeholder={name}
+              />
+            </div>
+            <div className="form-group">
+              <label forhtml="Address">Address</label>
+              <input
+                type="text"
+                className="form-control"
+                onChange={this.getAddress}
+                placeholder={address}
+              />
+            </div>
+            <div className="form-group">
+              <label forhtml="Phone">Phone</label>
+              <input
+                type="tel"
+                className="form-control"
+                onChange={this.getPhone}
+                placeholder={phone}
+              />
+            </div>
+            <div className="form-group">
+              <label forhtml="Avatar">Avatar</label>
+              <input
+                type="number"
+                min="1"
+                max="99"
+                className="form-control"
+                onChange={this.getAvatar}
+                placeholder={avatar}
+              />
+            </div>
+            <label forhtml="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              onChange={this.getEmail}
+              placeholder={email}
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" forhtml="exampleCheck1">
+              Men True Women False
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Save
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default EditContact;
---
+++ src/index.js
@@ -0,0 +1,182 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import uuid from "react-uuid";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./index.css";
+
+// Components
+import ContactList from "./Components/ContactList/ContactList";
+import AddContact from "./Components/AddContact/AddContact";
+import Header from "./Components/Header/Header";
+import NotFound from "./Components/NotFound/NotFound";
+import EditContact from "./Components/EditContact/EditContact";
+
+class App extends React.Component {
+  state = {
+    List: [
+      {
+        id: uuid(),
+        name: "Richerd Stevens",
+        address: "5842 Hillcrest Rd",
+        phone: "(870) 288-4149",
+        email: "richerd.stevens@example.com",
+        gender: "men",
+        avatar: 3,
+        star: false
+      },
+      {
+        id: uuid(),
+        name: "Linus Torvalds",
+        address: "1236 Stepana Banderu street",
+        phone: "(068) 87-41-789",
+        email: "linus@kernel.org",
+        gender: "men",
+        avatar: 34,
+        star: true
+      },
+      {
+        id: uuid(),
+        name: "Deniss Richi",
+        address: "12 Pr. Pease",
+        phone: "(050) 288-41-491",
+        email: "deniss@example.com",
+        gender: "men",
+        avatar: 76,
+        star: true
+      },
+      {
+        id: uuid(),
+        name: "Camila terry",
+        address: "12 London",
+        phone: "(066) 77-61-291",
+        email: "Camila@london.com",
+        gender: "women",
+        avatar: 23,
+        star: false
+      }
+    ],
+    currentContact: ""
+  };
+
+  onStarChange = id => {
+    // console.log("onStarChange ", id);
+    const index = this.state.List.findIndex(elem => elem.id === id);
+    let temList = this.state.List.slice();
+    temList[index].star = !temList[index].star;
+    this.setState(state => {
+      return {
+        star: !this.temList
+      };
+    });
+  };
+
+  onAddContact = (name, address, phone, avatar, email) => {
+    // console.log(
+    //   `Name: ${name}\nAdderss: ${address}\nPhone: ${phone}\nAvatar: ${avatar}\nEmail: ${email}`
+    // );
+    const newContact = {
+      id: uuid(),
+      name: name,
+      address: address,
+      phone: phone,
+      email: email,
+      gender: "women",
+      avatar: avatar,
+      star: false
+    };
+    const newList = [...this.state.List, newContact];
+    this.setState({
+      List: newList
+    });
+  };
+  onDeleteContact = id => {
+    const index = this.state.List.findIndex(elem => elem.id === id);
+    // console.log(id);
+    // console.log(index);
+    const partOne = this.state.List.slice(0, index);
+    const partTwo = this.state.List.slice(index + 1);
+    const newList = [...partOne, ...partTwo];
+    this.setState({
+      List: newList
+    });
+  };
+
+  onEditContact = id => {
+    const index = this.state.List.findIndex(elem => elem.id === id);
+    const currentContact = this.state.List[index];
+    // console.log(currentContact);
+    this.setState({
+      currentContact: currentContact
+    });
+  };
+
+  onEditCurrentContact = (name, address, phone, avatar, email, id) => {
+    // console.log(
+    //   `Name: ${name}\nAdderss: ${address}\nPhone: ${phone}\nAvatar: ${avatar}\nEmail: ${email}`
+    // );
+    const index = this.state.List.findIndex(elem => elem.id === id);
+    const editedContact = {
+      id: id,
+      name: name,
+      address: address,
+      phone: phone,
+      email: email,
+      gender: "women",
+      avatar: avatar,
+      star: false
+    };
+    //console.log(editedContact);
+    const partOne = this.state.List.slice(0, index);
+    const partTwo = this.state.List.slice(index + 1);
+    const newList = [...partOne, editedContact, ...partTwo];
+    //console.log(newList);
+    this.setState({
+      List: newList
+    });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Router>
+          <Header />
+          <div className="container">
+            <div className="row">
+              <Switch>
+                <Route
+                  path="/"
+                  exact
+                  render={() => (
+                    <ContactList
+                      List={this.state.List}
+                      onStarChange={this.onStarChange}
+                      onDeleteContact={this.onDeleteContact}
+                      onEditContact={this.onEditContact}
+                    />
+                  )}
+                />
+                <Route
+                  path="/addcontact"
+                  exact
+                  render={() => <AddContact onAddContact={this.onAddContact} />}
+                />
+                <Route
+                  path="/edit"
+                  render={() => (
+                    <EditContact
+                      currentContact={this.state.currentContact}
+                      onEditCurrentContact={this.onEditCurrentContact}
+                    />
+                  )}
+                />
+                <Route path="*" component={NotFound} />>
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
